bcb1d33db8be3a7f664b85ff5e3084a6a78cc3ee
J. Austin Hughey <jaustinhughey@me.com>
2013-04-19 18:10:44 -0500

    specify valid options for preferences

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,41 @@ class User < ActiveRecord::Base
 
   # Require an email address at the bare minimum. Preferences can be blank.
   validates :email, :presence => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/ }
+
+  # Define which themes, languages and currencies are valid.
+  def self.valid_themes
+    [
+      "Dark",
+      "Light",
+      "Future"
+    ]
+  end
+
+  def self.valid_currencies
+    [
+      "USD",
+      "AUD",
+      "GBP",
+      "JPY",
+      "CDN",
+      "EUR",
+      "BTC"
+    ]
+  end
+
+  def self.valid_languages
+    [
+      "US English",
+      "UK English",
+      "Hindi",
+      "Aramaic",
+      "Arabic",
+      "Japanese",
+      "French",
+      "German",
+      "Italian",
+      "Hebrew",
+      "l33tsp34k"
+    ]
+  end
 end
