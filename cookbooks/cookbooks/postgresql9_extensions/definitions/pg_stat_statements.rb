6182efaa7d11b7069f6c67ddbf50b955519510fe
J. Austin Hughey <jaustinhughey@me.com>
2013-04-22 20:49:23 -0500

    fixing cookbooks

--- cookbooks/cookbooks/postgresql9_extensions/definitions/pg_stat_statements.rb
+++
@@ -1,9 +0,0 @@
-define :postgresql9_pg_stat_statements do
- dbname_to_use = params[:name]
-
-  load_sql_file do
-    db_name dbname_to_use
-    extname "pg_stat_statements"
-    supported_versions %w[9.2]
-  end
-end
--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -98,8 +98,8 @@
 if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # Extensions that support both Postgres 9.0, 9.1 and 9.2
   # postgresql9_autoexplain "dbname"
-  postgresql9_btree_gin "dbname"
-  postgresql9_btree_gist "dbname"
+  postgresql9_btree_gin "rails_hstore_example"
+  postgresql9_btree_gist "rails_hstore_example"
   # postgresql9_chkpass "dbname"
   # postgresql9_citext "dbname"
   # postgresql9_cube "dbname"
@@ -108,7 +108,7 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   # postgresql9_dict_xsyn "dbname"
   # postgresql9_earthdistance "dbname"
   # postgresql9_fuzzystrmatch "dbname"
-  postgresql9_hstore "dbname"
+  postgresql9_hstore "rails_hstore_example"
   # postgresql9_intarray "dbname"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
@@ -140,4 +140,4 @@ if ['solo','db_master', 'db_slave'].include?(node[:instance_role])
   #Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
-#end
+end
